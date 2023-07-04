17a0357e57750c8ffd1a245551fee695aafe6ebc
Lara Costa <larasmila64@gmail.com>
2023-07-04 13:02:28 -0300

    feat: list result of the request

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,44 +1,48 @@
-import { useEffect, useState } from "react";
-import {Container} from './styles';
-import {WiNightAltCloudyWindy} from 'react-icons/wi';
-import api from "../../services/api"
-// const baseURL = process.env.REACT_APP_URL;
+import {useState } from "react";
+import { Container } from "./styles";
+import { WiNightAltCloudyWindy } from "react-icons/wi";
+import api from "../../services/api";
+
 const apiKey = process.env.REACT_APP_API_KEY;
 
-
 export function Header() {
-  const [request, setRequest] = useState();
-  // const [data, setData]= useState([]);
+  const [location, setLocation] = useState("london");
+  const [weatherData, setWeatherData] = useState();
 
-  useEffect(()=>{
-    api.get('/current.json',{
-      params:{
-      key: apiKey,
-      q:'London',
-
-    }})
-    .then(response => {
-      const weatherData = response.data;
-      console.log(weatherData);
-    })
-    // .then(response => setData(response.data))
-    .catch(error => console.log(error))
-  },[])
-
+  const handleSearch = () => {
+    api
+      .get("/current.json", {
+        params: {
+          key: apiKey,
+          q: location,
+        },
+      })
+      .then((response) => {
+        const weatherData = response.data;
+        setWeatherData(weatherData);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <Container>
-        <WiNightAltCloudyWindy width="50px" height="50px"/>
+      <WiNightAltCloudyWindy />
       <div className="content">
-          <input
-            type="text"
-            placeholder="Location"
-            value={request}
-            onChange={(e) => setRequest(e.target.value)}
-          />
-          <button type="button" id="btnSubmit" onClick={() => console.log('clicou')}>
-            Search
-          </button>
+        <input
+          type="text"
+          placeholder="Location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button type="button" id="btnSubmit" onClick={handleSearch}>
+          Search
+        </button>
+        {weatherData && (
+          <div>
+            <p>Current temperature: {weatherData.current.temp_c} °C</p>
+            <p>Condition: {weatherData.current.condition.text}</p>
+          </div>
+        )}
       </div>
     </Container>
   );

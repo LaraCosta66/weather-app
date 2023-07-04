de7899eb808e7a7c3df21b693f6972a4f6444a31
Lara Costa <larasmila64@gmail.com>
2023-07-04 13:37:24 -0300

    feat: move logic of api to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import { Header } from "./components/Header";
+import { Main } from "./components/Main";
 import { GlobalStyle } from "./styles/global";
+import api from "./services/api";
+import { useState } from "react";
 
+const apiKey = process.env.REACT_APP_API_KEY;
 export function App() {
+  const [weatherData, setWeatherData] = useState();
+
+  const handleSearch = () => {
+    api
+      .get("/current.json", {
+        params: {
+          key: apiKey,
+          q: "london",
+        },
+      })
+      .then((response) => {
+        const weatherData = response.data;
+        setWeatherData(weatherData);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <>
-      <Header />
+      <Header handleSearch={handleSearch} />
+      {weatherData && <Main weatherData={weatherData} />}
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,32 +1,11 @@
-import {useState } from "react";
+import { useState } from "react";
 import { Container } from "./styles";
-import { WiNightAltCloudyWindy } from "react-icons/wi";
-import api from "../../services/api";
 
-const apiKey = process.env.REACT_APP_API_KEY;
-
-export function Header() {
-  const [location, setLocation] = useState("london");
-  const [weatherData, setWeatherData] = useState();
-
-  const handleSearch = () => {
-    api
-      .get("/current.json", {
-        params: {
-          key: apiKey,
-          q: location,
-        },
-      })
-      .then((response) => {
-        const weatherData = response.data;
-        setWeatherData(weatherData);
-      })
-      .catch((error) => console.log(error));
-  };
+export function Header({ handleSearch }) {
+  const [location, setLocation] = useState("");
 
   return (
     <Container>
-      <WiNightAltCloudyWindy />
       <div className="content">
         <input
           type="text"
@@ -34,15 +13,9 @@ export function Header() {
           value={location}
           onChange={(e) => setLocation(e.target.value)}
         />
-        <button type="button" id="btnSubmit" onClick={handleSearch}>
+        <button type="button" id="btnSubmit" onClick={() => handleSearch()}>
           Search
         </button>
-        {weatherData && (
-          <div>
-            <p>Current temperature: {weatherData.current.temp_c} °C</p>
-            <p>Condition: {weatherData.current.condition.text}</p>
-          </div>
-        )}
       </div>
     </Container>
   );

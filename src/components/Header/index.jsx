b13cf27325945931f3baafbefa87cb321e57d8f5
Lara Costa <larasmila64@gmail.com>
2023-07-04 12:28:31 -0300

    feat: setting up axios request

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {Container} from './styles';
-
-const baseURL = process.env.REACT_APP_URL;
+import {WiNightAltCloudyWindy} from 'react-icons/wi';
+import api from "../../services/api"
+// const baseURL = process.env.REACT_APP_URL;
 const apiKey = process.env.REACT_APP_API_KEY;
 
 
 export function Header() {
   const [request, setRequest] = useState();
-  async function getCurrentWeather() {
-    const response = await fetch(`${baseURL}${apiKey}&q=${request}}`, { mode: 'cors' });
-    const responseData = await response.json();
-    return console.log(responseData);
+  // const [data, setData]= useState([]);
 
-  }
+  useEffect(()=>{
+    api.get('/current.json',{
+      params:{
+      key: apiKey,
+      q:'London',
+
+    }})
+    .then(response => {
+      const weatherData = response.data;
+      console.log(weatherData);
+    })
+    // .then(response => setData(response.data))
+    .catch(error => console.log(error))
+  },[])
+
 
   return (
     <Container>
-        <p>Imagem aqui</p>
+        <WiNightAltCloudyWindy width="50px" height="50px"/>
       <div className="content">
           <input
             type="text"
@@ -24,7 +36,7 @@ export function Header() {
             value={request}
             onChange={(e) => setRequest(e.target.value)}
           />
-          <button type="button" id="btnSubmit" onClick={getCurrentWeather}>
+          <button type="button" id="btnSubmit" onClick={() => console.log('clicou')}>
             Search
           </button>
       </div>
